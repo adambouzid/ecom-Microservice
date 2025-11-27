a022867bc564218d8480d6428709bbb9b46d99ce
Adam Bouzid <adambojzid2003@gmail.com>
2025-11-27 12:22:24 +0100

    Update project configuration and add discovery service

--- gateway-service/src/main/java/net/adam/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/net/adam/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,22 @@ package net.adam.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.config.client.ConfigClientProperties;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator Locator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
     }
 
 }
